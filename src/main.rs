0307a4cb1c77757b4378b75c197aed73fc55bad8
Leyang Zou <leyang.zou@student.tdsb.on.ca>
2021-06-30 20:24:08 -0400

    Write app code

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,80 @@
-use rickroll::lexer::*;
-use rickroll::parser::*;
-use rickroll::interpreter::*;
+use rickroll::lexer::Lexer;
+use rickroll::parser::Parser;
+use rickroll::interpreter::Interpreter;
 
-use std::env::args;
+use std::fs::File;
 use std::io::*;
+use std::path::PathBuf;
 
-fn main() {
-    let mut arguments = args();
-    arguments.next();
-    let raw = format!(
-        "\
-[Verse fib]
-(Ooh give you a)
-Inside we both know a <= 1
-  (Ooh) Never gonna give, never gonna give (give you a)
-Your heart's been aching but you're too shy to say it
-Never gonna let b down
-Never gonna let c down
-Never gonna give b a - 1
-Never gonna give c a - 2
-(Ooh give you b) Never gonna run fib and desert b
-(Ooh give you c) Never gonna run fib and desert c
-(Ooh) Never gonna give, never gonna give (give you b + c)
+use structopt::StructOpt;
+use ansi_term::Colour::Red;
 
-[Chorus]
-Never gonna let a down
-Never gonna give a {}
-(Ooh give you a) Never gonna run fib and desert a
-Never gonna say a
-    ",
-        arguments.next().unwrap()
-    );
-    println!("\"{}\"", raw);
-    eprintln!("\x1b[0;31mStarted lexing...\x1b[0m");
+#[derive(StructOpt, Debug)]
+struct Opt {
+    #[structopt(short, long, about="Print debugging information")]
+    debug: bool,
+    #[structopt(parse(from_os_str))]
+    file: PathBuf,
+}
+
+fn execute(file: PathBuf, debug: bool) -> std::result::Result<(), Error> {
+    // read from file
+    let mut f = File::open(file)?;
+    let mut raw = String::new();
+    f.read_to_string(&mut raw)?;
+    if debug {
+        eprintln!("{}", Red.paint("Started lexing..."));
+    }
     let lexer = Lexer::new(raw);
-    let ir = lexer.parse().unwrap();
-    println!("{:?}", ir);
-    eprintln!("\x1b[0;31mFinished lexing...\x1b[0m");
-    eprintln!("\x1b[0;31mStarted parsing...\x1b[0m");
-    let parser = Parser::new(ir);
-    let parsed = parser.parse().unwrap();
-    println!("{:?}", parsed);
-    eprintln!("\x1b[0;31mFinished parsing...\x1b[0m");
-    eprintln!("\x1b[0;31mStarted interpreting...\x1b[0m");
+    let tokens = lexer.parse();
+    match tokens {
+        Err(e) => {
+            eprintln!("{}", Red.paint(format!("{}", e)));
+            return Ok(());
+        }
+        _ => (),
+    };
+    let tokens = tokens.unwrap();
+    if debug {
+        println!("{:?}", tokens);
+        eprintln!("{}", Red.paint("Finished lexing..."));
+        eprintln!("{}", Red.paint("Started parsing..."));
+    }
+    let parser = Parser::new(tokens);
+    let parsed = parser.parse();
+    match parsed {
+        Err(e) => {
+            eprintln!("{}", Red.paint(format!("{}", e)));
+            return Ok(());
+        }
+        _ => (),
+    };
+    let parsed = parsed.unwrap();
+    if debug {
+        println!("{:?}", parsed);
+        eprintln!("{}", Red.paint("Finished parsing..."));
+        eprintln!("{}", Red.paint("Started executing..."));
+    }
     let mut interpreter = Interpreter::new(parsed);
-    println!(
-        "\n{:#?}",
-        interpreter.run(&mut stdout(), &mut BufReader::new(stdin()))
-    );
-    eprintln!("\x1b[0;31mFinished interpreting...\x1b[0m");
+    let result = interpreter.run(&mut stdout(), &mut BufReader::new(stdin()));
+    match result {
+        Err(e) => {
+            eprintln!("{}", Red.paint(format!("{}", e)));
+            return Ok(());
+        }
+        _ => (),
+    }
+    if debug {
+        println!(
+            "\n{:#?}",
+            result.unwrap()
+        );
+    }
+    return Ok(());
+}
+
+fn main() -> std::result::Result<(), Error> {
+    let opt = Opt::from_args();
+    execute(opt.file, opt.debug)?;
+    return Ok(());
 }
